6a3aacfec9d12cb0a97e0c60abe39ab7beb8259d
Unnati Gupta <unnati.gupta@6dtech.co.in>
2023-09-24 17:25:00 +0530

    add_docker_project

--- playground/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# def say_hello(request):
-#    return HttpResponse("Hello World")
-def say_hello(request):
-    return render(request,'index.html')
